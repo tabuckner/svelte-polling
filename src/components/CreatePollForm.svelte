3ed593f84518c04b4e656b656a85e3e561daf035
Taylor Buckner <taylora.buckner@gmail.com>
2022-03-30 16:49:00 -0500

    Added input validation to form.

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -6,9 +6,44 @@
     answerA: "",
     answerB: "",
   };
+  let errors = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+  let valid = false;
 
   const onSubmit = () => {
-    console.warn(fields);
+    valid = true;
+    
+    // validate question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at least 5 characters long.'
+    } else {
+      errors.question = ''
+    }
+    
+    // validate answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = 'Answer cannot be empty.'
+    } else {
+      errors.answerA = ''
+    }
+    
+    // validate Answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'Answer cannot be empty.'
+    } else {
+      errors.answerB = ''
+    }
+
+    // Add new poll
+    if (valid) {
+      console.warn('Form is valid.', fields)
+    }
   };
 </script>
 
@@ -16,14 +51,17 @@
   <div class="form-field">
     <label for="question">Poll Question</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{ errors.question }</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{ errors.answerA }</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{ errors.answerB }</div>
   </div>
 
   <Button type="secondary" flat="true" inverse="true">Add Poll</Button>
@@ -48,5 +86,11 @@
   label {
     margin: 8px auto;
     text-align: left;
+  }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
